b33a3025e109c16f998961aa6664ed955e314da2
Martin Ojeda <pmojeda@gmail.com>
2022-04-27 16:33:01 -0300

    Updates

---
+++ precache-manifest.5aeea62871b58523b6be4a065380bf60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "397c4188523317a39237",
+    "url": "/FitShopOjeda/static/css/main.0afc5716.chunk.css"
+  },
+  {
+    "revision": "397c4188523317a39237",
+    "url": "/FitShopOjeda/static/js/main.397c4188.chunk.js"
+  },
+  {
+    "revision": "5fc6eca808e7c1951395",
+    "url": "/FitShopOjeda/static/js/1.5fc6eca8.chunk.js"
+  },
+  {
+    "revision": "23f130685b74c80f0f8a",
+    "url": "/FitShopOjeda/static/js/2.23f13068.chunk.js"
+  },
+  {
+    "revision": "25d06ffd493da67979ba",
+    "url": "/FitShopOjeda/static/js/runtime~main.25d06ffd.js"
+  },
+  {
+    "revision": "42fe8a38fee5a1d29ac0ef4839416661",
+    "url": "/FitShopOjeda/index.html"
+  }
+];
